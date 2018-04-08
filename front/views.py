2871e990f711519281d92fc09d81942ff67b265f
Jesse Preiner <jezzah@gmail.com>
2018-04-08 13:39:15 -0600

    adds template files

--- front/views.py
+++ front/views.py
@@ -1,5 +1,18 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("<html><style>h1 {font-size: 72px; color: red; border: 1px solid blue;} ul {display: inline; list-style: none;}</style><ul>Links<li><a href='www.google.com'>About</a></li><li><a href='#'>Contact</a></li></ul><h1>My page</h1><div><img width=500 src='https://i.ytimg.com/vi/SfLV8hD7zX4/maxresdefault.jpg' /> </div></html>")
+    return HttpResponse("<html><style>h1 {font-size: 72px; color: red; border: 1px solid blue;} ul {display: inline; list-style: none;}</style><ul>Links<li><a href='./about'>About</a></li><li><a href='./contact'>Contact</a></li></ul><h1>My page</h1><div><img width=500 src='https://i.ytimg.com/vi/SfLV8hD7zX4/maxresdefault.jpg' /> </div></html>")
+
+def contact(request):
+    context = {
+        street: '1109 12th St E',
+        city: 'Saskatoon',
+        country: 'Canada',
+        phone: '3063618117',
+        email: 'jennifer@playsask.com'
+    }
+    return render(request, 'front/contact.html', context)
+
+def about(request):
+    return HttpResponse("I'm a twin")
