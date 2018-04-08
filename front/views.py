f5b969b7274e6fa171be5e9ac3f778913557cc2b
Jesse Preiner <jezzah@gmail.com>
2018-04-08 14:49:13 -0600

    fix dictionary syntax, remove /front url requirement

--- front/views.py
+++ front/views.py
@@ -6,11 +6,11 @@ def index(request):
 
 def contact(request):
     context = {
-        street: '1109 12th St E',
-        city: 'Saskatoon',
-        country: 'Canada',
-        phone: '3063618117',
-        email: 'jennifer@playsask.com'
+        'street': '1109 12th St E',
+        'city': 'Saskatoon',
+        'country': 'Canada',
+        'phone': '3063618117',
+        'email': 'jennifer@playsask.com'
     }
     return render(request, 'front/contact.html', context)
 
