f8134df12e710c47e86da8ac872bf3b1f331f86c
Jesse Preiner <jezzah@gmail.com>
2018-04-16 18:22:46 -0600

    beginnings of a theme before tearing apart and figuring out build system

--- front/views.py
+++ front/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("<html><style>h1 {font-size: 72px; color: red; border: 1px solid blue;} ul {display: inline; list-style: none;}</style><ul>Links<li><a href='./about'>About</a></li><li><a href='./contact'>Contact</a></li></ul><h1>My page</h1><div><img width=500 src='https://i.ytimg.com/vi/SfLV8hD7zX4/maxresdefault.jpg' /> </div></html>")
+    return render(request, 'front/about.html', {})
 
 def contact(request):
     context = {
